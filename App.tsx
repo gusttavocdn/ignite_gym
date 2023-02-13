f05619d6ac810b416bad6099f674abde884978da
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-02-13 11:45:48 -0300

    feat: add default theme

--- App.tsx
+++ App.tsx
@@ -6,20 +6,21 @@ import {
 } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
 import { Loading } from '@components/Loading';
+import { THEME } from './src/theme';
 
 export default function App() {
   const [fontsLoading] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   if (fontsLoading) {
     return (
-      <NativeBaseProvider>
+      <NativeBaseProvider theme={THEME}>
         <Loading />
       </NativeBaseProvider>
     );
   }
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <View
         style={{
           flex: 1,
